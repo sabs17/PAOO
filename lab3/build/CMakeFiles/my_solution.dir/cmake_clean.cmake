1eca3a85c89f6e9ff54444e95b23c2054f034ac1
Sabina <sabina.cenda17@gmail.com>
2022-11-03 13:35:27 +0200

    Laborator S4

---
+++ lab3/build/CMakeFiles/my_solution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_solution.dir/lab3.cpp.o"
+  "my_solution"
+  "my_solution.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_solution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
